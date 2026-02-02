fbcd62fb34559fd8f87a1b4a7eb1e7bc3a2894c2
danboy12342 <58471239+danboy12342@users.noreply.github.com>
2026-02-02 17:18:31 +0000

    ex5

--- Milestones/Programs/ex5_hello/src/main.c
+++ Milestones/Programs/ex5_hello/src/main.c
@@ -1,6 +1,5 @@
 /*
-Program 4: Drive a seven-segment display to display 0-9 repeatedly
-position of a switch
+Program 5: Drive a seven-segment display to display HELLO
 Description: 
 Course: 1064
 Author: Daniel Mui
@@ -14,17 +13,12 @@ Date: 2/2/2026
 /* innit */
 static const struct device *gpio_port_a = DEVICE_DT_GET(DT_NODELABEL(gpioa));
 
-static const uint8_t digits[10] = {
-    0x3F, // 0
-    0x06, // 1
-    0x5B, // 2
-    0x4F, // 3
-    0x66, // 4
-    0x6D, // 5
-    0x7D, // 6
-    0x07, // 7
-    0x7F, // 8
-    0x6F  // 9
+static const uint8_t hello_chars[5] = {
+    0x76, // H
+    0x79, // E
+    0x38, // L
+    0x38, // L
+    0x3F  // O
 };
 
 int main(void)
@@ -34,17 +28,19 @@ int main(void)
         return 0;
     }
 
+    /* Configure pins PA0 - PA7 as output */
     for (int pin = 0; pin < 8; pin++) {
         gpio_pin_configure(gpio_port_a, pin, GPIO_OUTPUT_ACTIVE | GPIO_OUTPUT_LOW);
     }
 
     while (1) {
-        /* Loop through numbers 0 to 9 */
-        for (int i = 0; i < 10; i++) {
-
-            gpio_port_set_masked(gpio_port_a, 0xFF, digits[i]);
+        /* Loop through the HELLO array */
+        for (int i = 0; i < 5; i++) {
             
-            /* Wait for next number */
+            /* Write the character to the port */
+            gpio_port_set_masked(gpio_port_a, 0xFF, hello_chars[i]);
+            
+            /* Wait for 1 second */
             k_sleep(K_MSEC(1000));
         }
     }
